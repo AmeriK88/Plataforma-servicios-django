6f31b5d1a673357b20bd5923cb9832a9c34ce6f0
Jose <lanzaltura17@gmail.com>
2024-11-06 18:21:17 +0000

    Style: implementación de estilo/sobreescritura de los estilos bootstrap

--- next_service/settings.py
+++ next_service/settings.py
@@ -117,7 +117,11 @@ LOGOUT_REDIRECT_URL = 'home'  # Nombre de la URL donde quieres redirigir despué
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
