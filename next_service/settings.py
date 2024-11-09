2a75795cdd448e86a746297f0e50236251d53010
Jose <lanzaltura17@gmail.com>
2024-11-09 20:32:55 +0000

    Feat: internacionalización cambiado a español & mostrado mesnajes error/color

--- next_service/settings.py
+++ next_service/settings.py
@@ -103,7 +103,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
